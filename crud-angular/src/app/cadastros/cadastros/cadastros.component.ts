f90e5b5d33fb85c07695f1d64d99bd81ab6764ca
Elys Sanntos <62489007+ElysSanntos@users.noreply.github.com>
2022-05-18 12:07:48 -0300

    Criando a lista de cadastros
    Grid de tabela
    https://material.angular.io/components/table/overview
    
    Na interface se coloca todas as variaveis. No TS a interface existe apenas em tempo de desenvolvimento
    Criar um interface:
    ng g interface cadastros/models/cadastro

--- crud-angular/src/app/cadastros/cadastros/cadastros.component.ts
+++ crud-angular/src/app/cadastros/cadastros/cadastros.component.ts
@@ -1,15 +1,23 @@
+import { Cadastro } from './../models/cadastro';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastros',
   templateUrl: './cadastros.component.html',
-  styleUrls: ['./cadastros.component.scss']
+  styleUrls: ['./cadastros.component.scss'],
 })
 export class CadastrosComponent implements OnInit {
+  // Devido ao modo strict estar como true, temos obrigatóriamente que iniciar essa variavel, o que tbm pode ser feito no constructor.
+  cadastros: Cadastro[] = [
+    {_id: '1',nome: 'Elys Sanntos',categoria: 'Pessoa Fisíca'},
+    {_id: '2',nome: 'Cleiton Peres',categoria: 'Pessoa Juridica'},
 
-  constructor() { }
+  ];
 
-  ngOnInit(): void {
-  }
+  //Colunas que serão exibidas
+  displayedColumns = ['_id', 'nome', 'categoria'];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
