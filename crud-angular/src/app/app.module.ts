56a762ecc5a55d3e8c880aea89cc67136f51d2c7
Elys Sanntos <62489007+ElysSanntos@users.noreply.github.com>
2022-05-24 21:29:31 -0300

    Update app.module.ts

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
