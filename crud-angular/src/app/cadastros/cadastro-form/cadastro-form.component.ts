f87c7c455058ca0cadf655b3e9cc825abd381413
Elys Sanntos <62489007+ElysSanntos@users.noreply.github.com>
2022-05-25 14:17:21 -0300

    Created Cadastro
    finalizado com sucesso

--- crud-angular/src/app/cadastros/cadastro-form/cadastro-form.component.ts
+++ crud-angular/src/app/cadastros/cadastro-form/cadastro-form.component.ts
@@ -1,5 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CadastrosService } from './../services/cadastros.service';
 
@@ -9,27 +11,39 @@ import { CadastrosService } from './../services/cadastros.service';
   styleUrls: ['./cadastro-form.component.scss'],
 })
 export class CadastroFormComponent implements OnInit {
-
   form: FormGroup;
-
 
   constructor(
     private formBuilder: FormBuilder,
-    private service:CadastrosService,
-    ) {
-
+    private service: CadastrosService,
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {
     this.form = this.formBuilder.group({
-      nome:[null],
-      categoria:[null]
+      nome: [null],
+      categoria: [null],
     });
   }
 
   ngOnInit(): void {}
 
-  onSubmit(){
-    //console.log(this.form.value);
-    this.service.save(this.form.value)
-    .subscribe(resultado => console.log(resultado));
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (resultado) => this.onSuccess(),
+      (error) => this.onError()
+    );
   }
-  onCancel(){}
+
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Cadastro realizado com sucesso!', '', { duration: 5000 });
+    this.onCancel();
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar o cadastro.', '', { duration: 5000 });
+  }
 }
--- crud-spring/src/main/java/com/cotuca/controller/CadastroController.java
+++ crud-spring/src/main/java/com/cotuca/controller/CadastroController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import com.cotuca.model.Cadastro;
 import com.cotuca.repository.CadastroRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +25,15 @@ public class CadastroController {
     @GetMapping
     public List<Cadastro>lista(){
         return cadastroRepositorio.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Cadastro> create(@RequestBody Cadastro registro){
+        //System.out.println(registro.getNome());
+        //return cadastroRepositorio.save(registro);
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(cadastroRepositorio.save(registro));
+
     }
     
 }
